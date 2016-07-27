a99f159e6c51df2deb6f85440cab871e0cf39204
Developer <devuser@ec2-52-58-60-108.eu-central-1.compute.amazonaws.com>
2016-07-27 12:10:33 +0000

    added info for production environment

--- bin/test/enroll.js
+++ bin/test/enroll.js
@@ -11,12 +11,14 @@ var buf2base64 = buf2.toString('base64');
 
 
 var requestJson = {
-    api_key: "51f6b93bf5f20e192195e9ac9879fd98", // DataReveal OnyxMeteor-dev
+//    api_key: "51f6b93bf5f20e192195e9ac9879fd98", // DataReveal OnyxMeteor-dev
+    api_key: "f2599a82c7eb8bd383a47f836e63aaf2", // DataReveal OnyxMeteor  
     template: buf1base64
 };
 
 request({
-    url: "http://localhost:3030/api/v1/onyx/enroll",
+//    url: "http://localhost:3030/api/v1/onyx/enroll", // config/development/env.sh
+    url: "http://localhost:3330/api/v1/onyx/enroll", // config/production/env.sh
     method: "POST",
     json: true,   // <--Very important!!!
     body: requestJson
--- bin/test/verify.js
+++ bin/test/verify.js
@@ -11,13 +11,16 @@ var buf2base64 = buf2.toString('base64');
 
 
 var requestJson = {
-    api_key: "51f6b93bf5f20e192195e9ac9879fd98", // DataReveal OnyxMeteor-dev
+//    api_key: "51f6b93bf5f20e192195e9ac9879fd98", // DataReveal OnyxMeteor-dev
+    api_key: "f2599a82c7eb8bd383a47f836e63aaf2", // DataReveal OnyxMeteor
     template: buf1base64,
-    userId: "SNDt6kuesxZh7HkgA"
+//    userId: "SNDt6kuesxZh7HkgA" // dev
+    userId: "xigEFwbcEDCBe5zJG" // prod
 };
 
 request({
-    url: "http://localhost:3030/api/v1/onyx/verify",
+//    url: "http://localhost:3030/api/v1/onyx/verify", // config/development/env.sh
+    url: "http://localhost:3330/api/v1/onyx/verify", // config/production/env.sh
     method: "POST",
     json: true,   // <--Very important!!!
     body: requestJson
